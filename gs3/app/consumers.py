2813c4850bfde1363fb43f309e7f4a6bced62dc4
DeepakAdoor05 <deepakadoor726@gmail.com>
2025-01-16 15:41:30 +0530

    AsyncConsumer code added

--- gs3/app/consumers.py
+++ gs3/app/consumers.py
@@ -1,4 +1,4 @@
-from channels.consumer import SyncConsumer
+from channels.consumer import SyncConsumer,AsyncConsumer
 class MySyncConsumer(SyncConsumer):
 
     def websocket_connect(self,event):  # predefined hanlers
@@ -11,4 +11,18 @@ class MySyncConsumer(SyncConsumer):
 
     def websocket_disconnect(self,event):  # predefined hanlers
         print("Websocket Disconnected...",event)
-    # This handler is called when either connection to the client is lost, either from the client closing the connection, or loss of the socket.
+    # This handler is called when either connection to the client is lost, either from the client closing the connection, or loss of the socket
+
+class MyASyncConsumer(AsyncConsumer):
+
+    async def websocket_connect(self,event):  # predefined hanlers
+        print("Websocket Connected...",event)
+    # 'websocket_connect' handler is called when client initially opens a connection and is about to finish the websocket handshake.
+
+    async def websocket_recieve(self,event):  # predefined hanlers
+        print("Message Recieved...",event)
+    # This handler is called when a data received from the client.
+
+    async def websocket_disconnect(self,event):  # predefined hanlers
+        print("Websocket Disconnected...",event)
+    # This handler is called when either connection to the client is lost, either from the client closing the connection, or loss of the socket
