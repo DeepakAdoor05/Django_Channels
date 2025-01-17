cd39301797e2c1dfb4510240d15723c4723b9002
DeepakAdoor05 <deepakadoor726@gmail.com>
2025-01-17 23:53:35 +0530

    Real time data from AsyncConsumer

--- gs3/app/consumers.py
+++ gs3/app/consumers.py
@@ -1,6 +1,7 @@
 from channels.consumer import SyncConsumer,AsyncConsumer
 from channels.exceptions import StopConsumer
 from time import sleep
+import asyncio
 class MySyncConsumer(SyncConsumer):
 
     def websocket_connect(self,event):  # predefined hanlers
@@ -11,7 +12,7 @@ class MySyncConsumer(SyncConsumer):
     # 'websocket_connect' handler is called when client initially opens a connection and is about to finish the websocket handshake.
 
     def websocket_receive(self,event):  # predefined hanlers
-        print("Message received from Client...",event)  # Here the server receives message from the client
+        print("Message received from Client",event)  # Here the server receives message from the client
         print(event['text'])
         for i in range(50):
             self.send({
@@ -37,11 +38,14 @@ class MyASyncConsumer(AsyncConsumer):
     # 'websocket_connect' handler is called when client initially opens a connection and is about to finish the websocket handshake.
 
     async def websocket_receive(self,event):  # predefined hanlers
-        print("Message received from Client...",event['text'])  # Here the server receives message from the client
-        await self.send({
-            'type' : "websocket.send",
-            'text' : "Message sent to Client"
-        }) # Here the message is sent to the client by the server
+        print("Message received from Client",event)  # Here the server receives message from the client
+        print(event['text'])
+        for i in range(50):
+            await self.send({
+                'type' : "websocket.send",
+                'text' : str(i)
+            }) # Here the message is sent to the client by the server
+            await asyncio.sleep(1) # Slow down 1s after each iteration.
     # This handler is called when a data received from the client.
 
     async def websocket_disconnect(self,event):  # predefined hanlers
