e4daf44c4708bfeb6938f4b64ef0172144193dfe
DeepakAdoor05 <deepakadoor726@gmail.com>
2025-01-26 00:10:13 +0530

    passing group name from view to index.html

--- gs9/app/views.py
+++ gs9/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request,group_name):
+    print("Group Name...",group_name)
+    return render(request,'app/index.html',{'groupname':group_name})
+# Passing the group name from views to template
+    
