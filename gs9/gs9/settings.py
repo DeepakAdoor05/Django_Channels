f3e5754b20f4886a172efba99900d4cf384b1f41
DeepakAdoor05 <deepakadoor726@gmail.com>
2025-01-25 23:59:02 +0530

    redis channel layer configuration included

--- gs9/gs9/settings.py
+++ gs9/gs9/settings.py
@@ -124,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuring redis CHANNEL_LAYERS
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("localhost", 6379)],
+        },
+    },
+}
