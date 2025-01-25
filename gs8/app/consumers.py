ac2b8d2e022dafa6bf4dae40694da0d5190341c8
DeepakAdoor05 <deepakadoor726@gmail.com>
2025-01-25 13:23:00 +0530

    AsyncConsumer static chat app completed

--- gs8/app/consumers.py
+++ gs8/app/consumers.py
@@ -2,6 +2,8 @@
 from channels.consumer import SyncConsumer,AsyncConsumer
 from channels.exceptions  import StopConsumer
 from asgiref.sync import async_to_sync
+import asyncio
+
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self,event):
         print('WebSocket Connected...',event)
@@ -43,4 +45,47 @@ class MySyncConsumer(SyncConsumer):
             'programmers',
             self.channel_name
             )
+        raise StopConsumer()
+
+class MyAsyncConsumer(AsyncConsumer):
+    async def websocket_connect(self,event):
+        print('WebSocket Connected...',event)
+        print("Channel Layer",self.channel_layer)   # gets the default channel_layer from a project.
+        print("Channel Name",self.channel_name)     # get channel name.
+        await self.channel_layer.group_add(
+            'programmers',
+            self.channel_name
+            )
+        await self.send({
+            'type' : "websocket.accept",
+            'text' : 'Hi from server'
+        })
+
+    async def websocket_receive(self,event):
+        print('Message Received from client...',event['text'])
+        print('Type of Message Received from client...',type(event['text']))
+        await self.channel_layer.group_send(
+            'programmers',
+            {
+                'type' : 'chat.message',
+                'message' : event['text']
+            }
+        )
+    async def chat_message(self,event):
+        print('Event...',event)
+        print('Actual Data...',event['message'])
+        print('Type of Actual data...',type(event['message']))
+        await self.send({
+            'type' : 'websocket.send',
+            'text' : event['message']
+        })
+
+    async def websocket_disconnect(self,event):
+        print('WebSocket Disconnected...',event)
+        print("Channel Layer",self.channel_layer)   # gets the default channel_layer from a project.
+        print("Channel Name",self.channel_name)     # get channel name.
+        await self.channel_layer.group_discard(
+            'programmers',
+             self.channel_name
+        )
         raise StopConsumer()
